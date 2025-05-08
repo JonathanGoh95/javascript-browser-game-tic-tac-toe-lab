7fb259929d79321dc1f2377b83b7527e10ab7d29
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-05-08 16:03:33 +0800

    Initial Setup

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*-------------------------------- Constants --------------------------------*/
+const section = document.querySelector(".board");
+const message = document.getElementById("message");
+const circle = document.getElementById("circle");
+const cross = document.getElementById("cross");
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+const startGame = () => {};
+const play = () => {};
+/*----------------------------- Event Listeners -----------------------------*/
+section.addEventListener("click", play);
+message.addEventListener("click", startGame);
